c381ef7c413fe83c278960d97643ebbdac0566db
Pamela Lovett <pamela.s.lovett@gmail.com>
2017-03-29 16:13:42 -0600

    Add mapdispatch to props.

--- app/components/App/AppContainer.js
+++ app/components/App/AppContainer.js
@@ -1,10 +1,18 @@
 import App from './app';
 import { connect } from 'react-redux';
+import * as actions from '../../actions/actions'
 
 
 const mapStateToProps = (state) => {
   return state
 }
 
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addMovies: (movie) => {
+      dispatch(actions.addMovies(movie))
+    }
+  }
+}
 
-export connect(mapStateToProps, )
+export default connect(mapStateToProps, mapDispatchToProps)(App)
