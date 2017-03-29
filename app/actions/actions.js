92e1f16d22a83ee7fbaf9bb4a81afc92abbbb94d
Hugh Morrison <hmorri32@gmail.com>
2017-03-29 15:18:05 -0600

    set up redux yall

---
+++ app/actions/actions.js
@@ -0,0 +1,5 @@
+export const addMovie = (movie) => {
+  return {
+    movie
+  }
+}
--- app/index.js
+++ app/index.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import createHistory from 'history/createBrowserHistory'
+import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux';
+
+import * as reducers from './reducers/reducers'
 import App from './components/App/app.jsx';
 import MovieGrid from './components/MovieGrid/MovieGrid';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import './assets/styles/main.css';
 
+const history    = createHistory()
+const devTools   = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const middleware = routerMiddleware(history)
+
+const root       = combineReducers({
+  router: routerReducer
+})
+
+const store = createStore(root, devTools, applyMiddleware(middleware))
 
 const router = (
-  <Router history={browserHistory} >
-    <Route path='/' component={ App }>
-    </Route>
-  </Router>
+  <Provider store={ store}>
+    <ConnectedRouter history={history} >
+      <App />
+    </ConnectedRouter>
+  </Provider>
 )
 
 render(router, document.getElementById('main'))
+
