07850fad246023dd9bf9c20c39c7ae8cc8b214c1
Tim <tbrandle53@gmail.com>
2017-03-29 18:23:25 -0600

    minor changes

--- app/components/Movie/Movie.js
+++ app/components/Movie/Movie.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import '../../assets/styles/movie.css';
+import {Link} from 'react-router-dom';
 
 const Movie = ({title, poster, vote}) => {
 
 console.log(poster);
   return (
     <div className="movie-card" style={ {backgroundImage: `url(https://image.tmdb.org/t/p/w342/${poster})` } }>
-      <h2 className="movie-title">{title}</h2>
-      <p className="movie-vote">{vote}</p>
     </div>
   )
 }
