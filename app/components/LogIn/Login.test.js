68aa4c87e0257af1f6a39828b8b4798cddce2e00
Kenia Farias <11431kenia@gmail.com>
2017-04-02 13:47:36 -0600

    testing setup

---
+++ app/components/LogIn/Login.test.js
@@ -0,0 +1,44 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import fetchMock from 'fetch-mock';
+import LogIn from './LogIn';
+
+
+
+describe('', () => {
+const mockUser ={
+  user: {
+    id: 1,
+    name: 'Taylor',
+    password:'password',
+    email: 'tman2272@aol.com'
+  }
+}
+
+const wrapper = shallow(
+  <LogIn signIn={jest.fn()} user={mockUser}/>
+)
+
+  it('displays error', async (done) => {
+    fetchMock.post('https://api.themoviedb.org/3/movie/now_playing?api_key=e918cc56cafd311d7955d426f4da1685&language=en-US&page=1', { status: 500, body: {} })
+
+    let emailInput = wrapper.find('input[name: "email"]');
+    let submitBtn = wrapper.find('button');
+
+    emailInput.simulate('change', {
+      target: {
+        value: 'buttz'
+      }
+    })
+
+    submitBtn.simulate('click');
+
+    await wrapper.update();
+
+    let expectedErrorMessage = 'invalid credentials';
+
+    expect(wrapper.state().error).toEqual(expectedErrorMessage);
+
+    done();
+  })
+})
