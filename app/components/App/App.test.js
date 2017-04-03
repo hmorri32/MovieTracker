00310eb0b5bbeadac3d5000380dd1fe4eb818dd9
Hugh Morrison <hmorri32@gmail.com>
2017-04-02 21:05:09 -0600

    reducers for actions and other such stuff

--- app/components/App/App.test.js
+++ app/components/App/App.test.js
@@ -1,51 +1,44 @@
-// import { shallow, mount } from 'enzyme';
-// import React from 'react';
-// import configureMockStore from 'redux-mock-store';
-// import fetchMock from 'fetch-mock';
-// import createHistory from 'history/createBrowserHistory';
-//
-//
-//
-// import stubMovies from '../../helpers/newMoviesStub.json'
-//
-// import App from './App.jsx';
-// import AppContainer from './AppContainer.js';
-//
-// const mockStore = configureMockStore()({})
-//
-//
-//
-// // need to test if state was passed down to props
-// // const setup = () => {
-// //   const Container = mount(
-// //     <Provider store={mockStore}>
-// //       <AppContainer />
-// //     </Provider>
-// //   );
-//
-//   const Component = Container.find(App);
-//
-//   return {
-//     Container,
-//     Component
-//   }
-// }
-//
-//
-// describe('app', () => {
-//
-//   const { Container, Component } = setup();
-//
-//
-//   it.only('should work', () => {
-//     // fetchMock.post('https://api.themoviedb.org/3/movie/now_playing?api_key=e918cc56cafd311d7955d426f4da1685&language=en-US&page=1', stubMovies )
-//     let wrapper = Component;
-//
-//     // await wrapper.update()
-//     console.log(wrapper);
-//
-//
-//     // done()
-//
-//   })
-// })
+import { shallow, mount } from 'enzyme';
+import React from 'react';
+import configureMockStore from 'redux-mock-store';
+import fetchMock from 'fetch-mock';
+import createHistory from 'history/createBrowserHistory';
+
+import stubMovies from '../../helpers/newMoviesStub.json'
+
+import App from './App.jsx';
+import AppContainer from './AppContainer.js';
+
+const mockStore = configureMockStore()({})
+
+
+
+// need to test if state was passed down to props
+// const setup = () => {
+//   const Container = mount(
+//     <Provider store={mockStore}>
+//       <AppContainer />
+//     </Provider>
+//   );
+
+
+
+
+
+describe.skip('app', () => {
+
+  const { Container, Component } = setup();
+
+
+  it.only('should work', () => {
+    // fetchMock.post('https://api.themoviedb.org/3/movie/now_playing?api_key=e918cc56cafd311d7955d426f4da1685&language=en-US&page=1', stubMovies )
+    let wrapper = Component;
+
+    // await wrapper.update()
+    console.log(wrapper);
+
+
+    // done()
+
+  })
+})
