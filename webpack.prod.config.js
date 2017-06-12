719a02bf670e36c348c342957e2ac91c13712893
Tim <tbrandle53@gmail.com>
2017-06-12 12:59:39 -0600

    Add webpack production file. Changed build script to webapck.prod

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -19,7 +19,7 @@ module.exports = {
        include: path.join(__dirname, 'app'),
        exclude: /node_modules/,
        query: {
-         presets: ['es2015', 'react', 'stage-0']
+         presets: ['es2015', 'react']
        }
      },
    { test: /\.css$/, loader: 'style!css' },
