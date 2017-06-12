a0dd62937f596313c108b9b99eeb41081741f5a8
Tim <tbrandle53@gmail.com>
2017-06-12 12:26:17 -0600

    Resturcture files

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('express-cors');
 var bodyParser = require('body-parser')
 const port = (process.env.PORT || 3000);
 const app = express();
-const users = require('./users');
+const users = require('./routes/users');
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV !== 'production') {
   const webpack = require('webpack');
   const webpackDevMiddleware = require('webpack-dev-middleware');
   const webpackHotMiddleware = require('webpack-hot-middleware');
-  const config = require('../webpack.config.js');
+  const config = require('./webpack.config.js');
   const compiler = webpack(config);
 
   app.use(webpackHotMiddleware(compiler));
@@ -25,12 +25,12 @@ if (process.env.NODE_ENV !== 'production') {
   }));
 }
 
-app.use('/assets', express.static(path.join(__dirname, '../app/assets')));
+app.use('/assets', express.static(path.join(__dirname, './app/assets')));
 
-app.get('/', function (req, res) { res.sendFile(path.join(__dirname, '/../index.html')) });
+app.get('/', function (req, res) { res.sendFile(path.join(__dirname, './index.html')) });
 
 app.use('/api', users);
-app.get('/*', function (req, res) { res.sendFile(path.join(__dirname, '/../index.html')) });
+app.get('/*', function (req, res) { res.sendFile(path.join(__dirname, './index.html')) });
 
 app.listen(port);
 
